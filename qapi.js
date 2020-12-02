8c3071e631825ec055d0a30b3542ee6873446e21
Goalof <68434460+Goalof@users.noreply.github.com>
2020-12-02 21:23:54 +0600

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,25 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5fc7b13b00ea36001e3b53a0",
+				"5fc7b13b00ea36001e3b53a2"
+			]
+		},
+		"5fc7b13b00ea36001e3b53a0": {
+			"id": "5fc7b13b00ea36001e3b53a0",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5fc7b13b00ea36001e3b53a2": {
+			"id": "5fc7b13b00ea36001e3b53a2",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby"
+}
---
+++ src/pages/index.js
@@ -0,0 +1,46 @@
+import React from "react";
+import theme from "theme";
+import { Theme, Link, Text } from "@quarkly/widgets";
+import { Helmet } from "react-helmet";
+import { Section } from "@quarkly/components";
+export default (() => {
+	return <Theme theme={theme}>
+		<Helmet>
+			<title>
+				Quarkly export
+			</title>
+			<meta name={"description"} content={"Web site created using quarkly.io"} />
+			<link rel={"shortcut icon"} href={"https://uploads.quarkly.io/readme/cra/favicon-32x32.ico"} type={"image/x-icon"} />
+		</Helmet>
+		<Section>
+			<Text font="600 16px --fontFamily-googleMontserrat">
+				Some text
+			</Text>
+			<Text font="100 16px --fontFamily-googleMontserrat">
+				Some text
+			</Text>
+		</Section>
+		<Link
+			font={"--capture"}
+			font-size={"10px"}
+			position={"fixed"}
+			bottom={"12px"}
+			right={"12px"}
+			z-index={"4"}
+			border-radius={"4px"}
+			padding={"5px 12px 4px"}
+			background-color={"--dark"}
+			opacity={"0.6"}
+			hover-opacity={"1"}
+			color={"--light"}
+			cursor={"pointer"}
+			transition={"--opacityOut"}
+			quarkly-title={"Badge"}
+			text-decoration-line={"initial"}
+			href={"https://quarkly.io/"}
+			target={"_blank"}
+		>
+			Made on Quarkly
+		</Link>
+	</Theme>;
+});
